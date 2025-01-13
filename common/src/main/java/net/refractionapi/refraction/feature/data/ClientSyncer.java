860da76d5d2fb7b7aaff7247937ef6ef9ecebf12
ZeusIGN <ZeusCovert@gmail.com>
2025-01-13 18:05:44 +0200

    interceptors

--- common/src/main/java/net/refractionapi/refraction/feature/data/ClientSyncer.java
+++ common/src/main/java/net/refractionapi/refraction/feature/data/ClientSyncer.java
@@ -1,13 +1,14 @@
 package net.refractionapi.refraction.feature.data;
 
 import net.minecraft.network.FriendlyByteBuf;
+import org.apache.commons.lang3.function.TriFunction;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 
 public class ClientSyncer<T extends Syncable<?>> {
-
     public static final HashMap<Class<? extends Syncable<?>>, ClientSyncer<?>> SERIALIZERS = new HashMap<>();
+    private static final HashMap<Class<? extends Syncable<?>>,TriFunction<Integer, FriendlyByteBuf, FriendlyByteBuf, ? extends Syncable<?>>> interceptors = new HashMap<>(); // can be unloaded, so it's static
     private final HashMap<Integer, T> cache = new HashMap<>();
     private final Class<T> clazz;
 
@@ -22,10 +23,21 @@ public class ClientSyncer<T extends Syncable<?>> {
 
     @SuppressWarnings("unchecked")
     public <C> void handleSerializer(Class<C> clazz, int id, FriendlyByteBuf buf, FriendlyByteBuf constArgs) {
+        if (interceptors.containsKey(clazz)) {
+            T interceptor = (T) interceptors.get(clazz).apply(id, buf, constArgs);
+            if (interceptor != null) {
+                interceptor.onSync(buf, id);
+                return;
+            }
+        }
          this.cache.computeIfAbsent(id, (i) -> {
             T byteBuf = createConstructed((Class<T>) clazz, constArgs);
             return byteBuf == null ? createEmpty((Class<T>) clazz) : byteBuf;
         }).onSync(buf, id);
+    }
+
+    public static <C extends Syncable<C>> void setInterceptor(Class<C> clazz, TriFunction<Integer, FriendlyByteBuf, FriendlyByteBuf, C> output) {
+        interceptors.put(clazz, output);
     }
 
     private T createEmpty(Class<T> clazz) {
@@ -44,5 +56,4 @@ public class ClientSyncer<T extends Syncable<?>> {
             return null;
         }
     }
-
 }
